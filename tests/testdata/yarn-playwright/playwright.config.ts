d17aa5e10147d63084bb72f3e999a702a38d2f0e
Andrew Berry <andrew@furrypaws.ca>
2023-05-02 21:07:07 -0400

    Ignore TLS errors

--- tests/testdata/yarn-playwright/playwright.config.ts
+++ tests/testdata/yarn-playwright/playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    ignoreHTTPSErrors: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
