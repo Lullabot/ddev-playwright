4fb1491497cef2701bb79e0c9f74f840b0a8f8dd
Andrew Berry <andrew@furrypaws.ca>
2024-03-02 15:43:33 -0500

    Update kasmvnc for Debian 12

---
+++ web-build/install-kasmvnc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $(arch) == "aarch64" ]; then
+  KASM_ARCH=arm64
+else
+  KASM_ARCH=amd64
+fi;
+
+RELEASE=$(lsb_release --short --codename)
+
+wget https://github.com/kasmtech/KasmVNC/releases/download/v1.1.0/kasmvncserver_${RELEASE}_1.3.0_${KASM_ARCH}.deb'
+sudo apt-get install -y ./kasmvncserver*.deb
