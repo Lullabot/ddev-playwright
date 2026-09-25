bad73bb1d40bff6da5c1840a2c4dbfd0e4a182c4
Andrew Berry <andrew.berry@lullabot.com>
2026-09-25 13:32:46 -0700

    fix: clean up tmpfs browser profiles

--- tests/testdata/phpinfo.spec.ts
+++ tests/testdata/phpinfo.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('uses the add-on tmpfs for browser profiles', () => {
-  expect(process.env.TMPDIR).toBe('/tmp/sqlite/playwright-tmp');
+  expect(process.env.TMPDIR).toMatch(/^\/tmp\/sqlite\/playwright-tmp\./);
 });
 
 test('has title', async ({ page }) => {
